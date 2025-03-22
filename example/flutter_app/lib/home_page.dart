1028a2ebf84b0a9adfac16a2daadd89cec609fbb
suragch <suragch@suragch.dev>
2025-03-22 11:10:36 +0800

    show isol init medi fina

--- example/flutter_app/lib/home_page.dart
+++ example/flutter_app/lib/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart' hide Unicode;
 import 'package:flutter/material.dart';
+import 'package:mongol_code/mongol_code.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,14 +20,162 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Mongolian Characters')),
-      body: ListView.builder(
-        itemCount: mongolianCharacters.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(mongolianCharacters[index], style: const TextStyle(fontSize: 44)),
-            subtitle: Text('U+${(0x1820 + index).toRadixString(16).toUpperCase().padLeft(4, '0')}'),
-          );
-        },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: ListView.builder(
+          itemCount: mongolianCharacters.length,
+          itemBuilder: (context, index) {
+            final char = mongolianCharacters[index];
+            final fvs1 = String.fromCharCode(Unicode.FVS1);
+            final fvs2 = String.fromCharCode(Unicode.FVS2);
+            final fvs3 = String.fromCharCode(Unicode.FVS3);
+            final nirugu = String.fromCharCode(Unicode.MONGOLIAN_NIRUGU);
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text('U+${(0x1820 + index).toRadixString(16).toUpperCase()}'),
+                    SizedBox(width: 16),
+                    Text(char, style: const TextStyle(fontSize: 44)),
+                  ],
+                ),
+
+                Row(
+                  children: [
+                    SizedBox(width: 16),
+                    Text('ISOLATE', style: const TextStyle(fontSize: 20)),
+                    SizedBox(width: 16),
+                    Text(char, style: const TextStyle(fontSize: 44)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 32),
+                    Text('FVS1'),
+                    SizedBox(width: 16),
+                    Text('$char$fvs1', style: const TextStyle(fontSize: 32)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 32),
+                    Text('FVS2'),
+                    SizedBox(width: 16),
+                    Text('$char$fvs2', style: const TextStyle(fontSize: 32)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 32),
+                    Text('FVS3'),
+                    SizedBox(width: 16),
+                    Text('$char$fvs3', style: const TextStyle(fontSize: 32)),
+                  ],
+                ),
+
+                Row(
+                  children: [
+                    SizedBox(width: 16),
+                    Text('INITIAL', style: const TextStyle(fontSize: 20)),
+                    SizedBox(width: 16),
+                    Text('$char$nirugu', style: const TextStyle(fontSize: 44)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 32),
+                    Text('FVS1'),
+                    SizedBox(width: 16),
+                    Text('$char$fvs1$nirugu', style: const TextStyle(fontSize: 32)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 32),
+                    Text('FVS2'),
+                    SizedBox(width: 16),
+                    Text('$char$fvs2$nirugu', style: const TextStyle(fontSize: 32)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 32),
+                    Text('FVS3'),
+                    SizedBox(width: 16),
+                    Text('$char$fvs3$nirugu', style: const TextStyle(fontSize: 32)),
+                  ],
+                ),
+
+                Row(
+                  children: [
+                    SizedBox(width: 16),
+                    Text('MEDIAL', style: const TextStyle(fontSize: 20)),
+                    SizedBox(width: 16),
+                    Text('$nirugu$char$nirugu', style: const TextStyle(fontSize: 44)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 32),
+                    Text('FVS1'),
+                    SizedBox(width: 16),
+                    Text('$nirugu$char$fvs1$nirugu', style: const TextStyle(fontSize: 32)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 32),
+                    Text('FVS2'),
+                    SizedBox(width: 16),
+                    Text('$nirugu$char$fvs2$nirugu', style: const TextStyle(fontSize: 32)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 32),
+                    Text('FVS3'),
+                    SizedBox(width: 16),
+                    Text('$nirugu$char$fvs3$nirugu', style: const TextStyle(fontSize: 32)),
+                  ],
+                ),
+
+                Row(
+                  children: [
+                    SizedBox(width: 16),
+                    Text('FINAL', style: const TextStyle(fontSize: 20)),
+                    SizedBox(width: 16),
+                    Text('$nirugu$char', style: const TextStyle(fontSize: 44)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 32),
+                    Text('FVS1'),
+                    SizedBox(width: 16),
+                    Text('$nirugu$char$fvs1', style: const TextStyle(fontSize: 32)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 32),
+                    Text('FVS2'),
+                    SizedBox(width: 16),
+                    Text('$nirugu$char$fvs2', style: const TextStyle(fontSize: 32)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 32),
+                    Text('FVS3'),
+                    SizedBox(width: 16),
+                    Text('$nirugu$char$fvs3', style: const TextStyle(fontSize: 32)),
+                  ],
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
