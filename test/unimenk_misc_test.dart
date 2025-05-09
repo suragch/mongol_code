11c12a7809ece5bd57ef6263dd03a5b1161f5d2c
suragch <suragch@suragch.dev>
2025-05-09 19:26:42 +0800

    more unicode tests

---
+++ test/unimenk_misc_test.dart
@@ -0,0 +1,55 @@
+import 'package:mongol_code/alternative/convert.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Unicode -> Menksoft: Other random tests', () {
+    test('qmqShortTailM', () async {
+      final unicode = 'ᠬᠮᠬ';
+      final result = convertUnicodeToMenksoft(unicode);
+      final expected = '\uE2CE\uE2F4\uE2D6';
+      expect(result, expected);
+    });
+
+    test('qlqShortTailL', () async {
+      final unicode = 'ᠬᠯᠬ';
+      final result = convertUnicodeToMenksoft(unicode);
+      final expected = '\uE2CE\uE2FA\uE2D6';
+      expect(result, expected);
+    });
+
+    test('egnenBigG', () async {
+      final unicode = 'ᠡᠭᠨᠡᠨ';
+      final result = convertUnicodeToMenksoft(unicode);
+      final expected = '\uE271\uE2EB\uE2B7\uE276\uE2B5';
+      expect(result, expected);
+    });
+
+    test('egmenBigG', () async {
+      final unicode = 'ᠡᠭᠮᠡᠨ';
+      final result = convertUnicodeToMenksoft(unicode);
+      final expected = '\uE271\uE2EB\uE2F6\uE276\uE2B5';
+      expect(result, expected);
+    });
+
+    test('eglenBigG', () async {
+      final unicode = 'ᠡᠭᠯᠡᠨ';
+      final result = convertUnicodeToMenksoft(unicode);
+      final expected = '\uE271\uE2EB\uE2FC\uE276\uE2B5';
+      expect(result, expected);
+    });
+
+    test('egdenSmallG', () async {
+      final unicode = 'ᠡᠭᠳᠡᠨ';
+      final result = convertUnicodeToMenksoft(unicode);
+      final expected = '\uE271\uE2EF\uE313\uE276\uE2B5';
+      expect(result, expected);
+    });
+
+    test('egsenSmallG', () async {
+      final unicode = 'ᠡᠭᠰᠡᠨ';
+      final result = convertUnicodeToMenksoft(unicode);
+      final expected = '\uE271\uE2EF\uE301\uE276\uE2B5';
+      expect(result, expected);
+    });
+  });
+}
---
+++ test/unimenk_todd_test.dart
@@ -0,0 +1,41 @@
+import 'package:mongol_code/alternative/convert.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Unicode -> Menksoft: Todo Script', () {
+    test('todoAllTodoCode', () async {
+      final unicode = 'ᡐᡆᡑᡆ'; //
+      final result = convertUnicodeToMenksoft(unicode);
+      final expected = 'ᡐᡆᡑᡆ';
+      expect(result, expected);
+    });
+
+    test('todoMixedTodoMongolCode', () async {
+      final unicode = 'ᠰᡇᠷᡋᡇᠯᡓᡅᠯ\u1820ᡋ\u1820'; //
+      final result = convertUnicodeToMenksoft(unicode);
+      final expected = 'ᠰᡇᠷᡋᡇᠯᡓᡅᠯ\u1820ᡋ\u1820';
+      expect(result, expected);
+    });
+
+    test('todoZWJ', () async {
+      final unicode = '\u200Dᡐ\u200D'; //
+      final result = convertUnicodeToMenksoft(unicode);
+      final expected = '\u200Dᡐ\u200D';
+      expect(result, expected);
+    });
+
+    test('todoFVS', () async {
+      final unicode = '\u1847\u180B';
+      final result = convertUnicodeToMenksoft(unicode);
+      final expected = '\u1847\u180B';
+      expect(result, expected);
+    });
+
+    test('todoZwjFvsMix', () async {
+      final unicode = '\u200D\u1847\u180B\u200D';
+      final result = convertUnicodeToMenksoft(unicode);
+      final expected = '\u200D\u1847\u180B\u200D';
+      expect(result, expected);
+    });
+  });
+}
